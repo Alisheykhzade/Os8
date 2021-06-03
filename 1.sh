5d3a6e19fa7548147ff13e3f2edd78f64a53203a
Alisheykhzade <76804802+Alisheykhzade@users.noreply.github.com>
2021-06-03 13:58:47 +0430

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,6 @@
+chmod 397 filetest_name
+chmod 440 filetest_name
+chmod a=rx filetest2_name
+chmod g=w filetest2_name
+chmod r+w filetest2_name
+chmod u+g filetest2_name
---
+++ 2.sh
@@ -0,0 +1,3 @@
+chmod -v file_name
+chmod -R file_name
+chmod -f file_name
